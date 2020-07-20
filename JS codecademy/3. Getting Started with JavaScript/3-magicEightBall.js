cc922169f384b34c9e8d351f8142fdb1160b78a0
Mike Mercier <merciermk@macbook-pro-de-mike.home>
2020-07-20 19:06:19 +0200

    Exercise 3 done! eightBall

---
+++ JS codecademy/3. Getting Started with JavaScript/3-magicEightBall.js
@@ -0,0 +1,44 @@
+let userName = 'Mike';
+let userQuestion = 'Can i have a good day?' ;
+let randomNumber = Math.floor(Math.random()*8);
+let eightBall = '';
+
+if (userName) {
+  console.log(`Hello, ${userName}!`);
+} else {
+  console.log('Hello noName!');
+}
+
+console.log(`${userName} asked: ${userQuestion}`);
+
+switch(randomNumber) {
+  case 0:
+    eightBall = 'It is certain';
+    break;
+  case 1:
+    eightBall = 'It is decidedly so';
+    break;
+  case 2:
+    eightBall = 'Reply hazy try again';
+    break;
+  case 3:
+    eightBall = 'Cannot predict now';
+    break;
+  case 4:
+    eightBall = 'Do not count on it';
+    break;
+  case 5:
+    eightBall = 'My sources say no';
+    break;
+  case 6:
+    eightBall = 'Outlook not so good';
+    break;
+  case 7:
+    eightBall = 'Signs point to yes';
+    break;
+  default :
+    console.log('Something wrong');
+    break;
+}
+
+console.log(eightBall);
