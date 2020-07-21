fb775b50e72bc6ccb04035b047abdac7df337b29
Mike Mercier <merciermk@macbook-pro-de-mike.home>
2020-07-21 15:13:43 +0200

    Change rules for eighteen years old runners

--- JS codecademy/3. Getting Started with JavaScript/4-raceDay.js
+++ JS codecademy/3. Getting Started with JavaScript/4-raceDay.js
@@ -1,14 +1,18 @@
 let raceNumber = Math.floor(Math.random() * 1000);
-let earlyRegistered = true;
-let runnerAge = 16;
+let earlyRegistered = false;
+let runnerAge = 18;
 let runHour = '';
 
-if(runnerAge >= 18 && earlyRegistered){
+if(runnerAge > 18 && earlyRegistered){
   raceNumber += 1000;
   runHour = 'Your start at 9:30 am';
-}else if (runnerAge >= 18 && !earlyRegistered) {
+}else if (runnerAge > 18 && !earlyRegistered) {
   runHour = 'Your start at 11:00 am';
-}else {
+}else if (runnerAge === 18){
+  raceNumber = 'Not allowed';
+  runHour = 'You should see the registration desk.';
+}
+else {
   // Young registrant
   runHour = 'Your start at 12:30 pm';
 }
