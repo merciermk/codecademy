0185c79cf1174da4aa2af5c243b1b15540d3e594
Mike Mercier <contact@mikemercier.com>
2020-07-26 12:15:55 +0200

    exercise earthWeight: done!

--- JS codecademy/5/earthWeight.js
+++ JS codecademy/5/earthWeight.js
@@ -1,11 +1,18 @@
-function earthWeight(earthWeight, planet){
+function calculateWeight(earthWeight, planet){
     planet = planet.toLowerCase();
     if (planet === 'mercury'){
-        console.log(planet);
         return (earthWeight * 0.378);
-
+    }else if (planet === 'venus'){
+        return (earthWeight * 0.907);
+    }else if(planet === 'mars'){
+        return (earthWeight * 0.377);
+    }else if(planet === 'jupiter'){
+        return (earthWeight * 2.36);
+    }else if(planet === 'saturn'){
+        return (earthWeight * 0.916);
+    }else{
+        return 'Invalid Planet Entry. Try: Mercury, Venus, Mars, Jupiter, or Saturn.';
     }
-    console.log(planet);
 }
 
-console.log(earthWeight(72, 'MercUry'));
+console.log(calculateWeight(100, 'Jupiter'));
